2bc5ac098a3e8b9760756d2a45c98ed94bd5865a
Dharunya S S <151717489+Dharunya-2003@users.noreply.github.com>
2025-02-21 11:16:13 +0530

    Update orangehrmdemo.java

--- seleniumwebDriver/src/test/java/locators/orangehrmdemo.java
+++ seleniumwebDriver/src/test/java/locators/orangehrmdemo.java
@@ -1,6 +1,11 @@
 package locators;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class orangehrmdemo {
@@ -11,7 +16,24 @@ public class orangehrmdemo {
 				WebDriver driver= new ChromeDriver();
 				driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 				driver.manage().window().maximize();
+				
+				
+				//wait to handle the delays
+				 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
+				
+				
+				 driver.findElement(By.name("username")).sendKeys("Admin");
+				 driver.findElement(By.name("password")).sendKeys("admin123");
+				 driver.findElement(By.xpath("//*[@type='submit']")).click();
+			        boolean isDashboardDisplayed = driver.findElement(By.xpath("//h6[text()='Dashboard']")).isDisplayed();
+			        if (isDashboardDisplayed) {
+			            System.out.println("Login Successful - Dashboard is displayed!");
+			        } else {
+			            System.out.println("Login Failed - Dashboard is NOT displayed!");
+			        }
 
+				 
+				 
 	}
 
 	
