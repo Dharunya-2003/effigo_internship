b8479f3f4baee6287de7a42d98566802d0a3e66d
Dharunya S S <151717489+Dharunya-2003@users.noreply.github.com>
2025-02-21 12:03:13 +0530

    Update orangehrmdemo.java

--- seleniumwebDriver/src/test/java/locators/orangehrmdemo.java
+++ seleniumwebDriver/src/test/java/locators/orangehrmdemo.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class orangehrmdemo {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		
 				WebDriver driver= new ChromeDriver();
@@ -18,20 +18,32 @@ public class orangehrmdemo {
 				//wait to handle the delays
 				 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
 				
-				
+				// this is for login page
 				 driver.findElement(By.name("username")).sendKeys("Admin");
 				 driver.findElement(By.name("password")).sendKeys("admin123");
 				 driver.findElement(By.xpath("//*[@type='submit']")).click();
 			        boolean isDashboardDisplayed = driver.findElement(By.xpath("//h6[.='Dashboard']")).isDisplayed();
 			        if (isDashboardDisplayed) {
-			            System.out.println("Login Successful  Dashboard is displayed!");
+			            System.out.println("Login successful  dashboard is displayed");
 			        } else {
-			            System.out.println("Login Failed  Dashboard is NOT displayed!");
+			            System.out.println("Login failed");
 			        }
 			        
+			        //to wait  for 3sec before logout
+			        Thread.sleep(3000);
+			        
+			        // this is for logout page
 			        driver.findElement(By.xpath("//p[@class='oxd-userdropdown-name']")).click();
 			        driver.findElement(By.xpath("//a[.='Logout']")).click();
-			        			 
+			        boolean isloginpagedisplayed=driver.findElement(By.name("username")).isDisplayed();
+			        if (isloginpagedisplayed) {
+			            System.out.println("Logout Successful it is redirected to login page");
+			        } else {
+			            System.out.println("Logout failed");
+			        }
+			        
+
+				 
 				 
 	}
 
